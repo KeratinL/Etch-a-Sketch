bb6fe469489e2d3e4bc1ada64b1f60d4fd21fddd
TryhardCactus <austinslewis@yahoo.com>
2022-09-01 21:34:15 -0700

    Implement marginal opacity increase

--- java-script.js
+++ java-script.js
@@ -11,34 +11,71 @@ function createArray(promptInput){
     }
     return square;
 }
-let promptInput = getInput();
-const array = createArray(promptInput * promptInput);
 
 const container = document.querySelector('#container');
 
-container.setAttribute('style', 'display: flex; height: 960px; width: 960px; margin: auto; border: solid black 2px;');
+container.setAttribute('style', 'display: flex; flex: 1 1 0; height: 960px; width: 960px; margin: auto; border: solid black 2px;');
 container.style.flexWrap = 'wrap';
 
-array.forEach(() => {
+function removeAllChildNodes(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+let opacity = 0.0;
+function addOpacity() {
+    for (i = 0; i < 10; i++) {
+        return opacity += 0.10;
+    }
+}
+
+let uniqueClass = 0;
+function createUniqueClass(){
+    
+    uniqueClass += 1;
+    return uniqueClass;
+    
+}
+
+
+
+console.log(Math.floor(Math.random()*16777215).toString(16));
+
+let promptInput = '';
+let array = [''];
+const button = document.querySelector('button');
+    
+button.addEventListener('click', () => {
+    removeAllChildNodes(container);
+    promptInput = getInput();
+    array = createArray(promptInput * promptInput);
+    array.forEach(() => {
     const div = document.createElement('div');
-    div.classList.add('box');
-    let box = document.querySelector('box');
+    div.classList.add(`box${createUniqueClass()}`);    
+    elements = div.classList;
     container.appendChild(div);
     div.style.height = `${960 / promptInput + 0.01}px`;
     div.style.width = `${960 / promptInput}px`;
-   
-    div.addEventListener('mouseover', () => {
-        div.style.opacity = '1';
-    });
+       
+    for (i = 0; i < elements.length; i++){
+        elements[i].addEventListener('mouseover', () => {
+            elements[i].style.opacity = `${addOpacity()}`;
+        })
+    }
     /*
     div.addEventListener('transitionend', () => {
         div.style.opacity = '0';
     });
     */
+    });
 });
 
-const playing = document.querySelectorAll('.box');
-playing
+
+
+
+
+
 
 
 
